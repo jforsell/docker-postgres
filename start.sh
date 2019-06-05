34a7e4bb8fbf975b5294445c2cb59142a0790bb2
Johan Forsell <johan@forsell.name>
2019-06-05 10:56:43 +0100

    Install pgAdmin4 if not installed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Pulling postgres image..."
 docker pull postgres
@@ -11,3 +11,9 @@ fi
 echo "Starting Postgres container..."
 docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
 
+if brew cask ls --versions pgadmin4 > /dev/null; then
+  echo "pgAdmin 4 already installed"
+else
+  echo "Installing pgAdmin 4"
+  brew cask install pgadmin4
+fi
